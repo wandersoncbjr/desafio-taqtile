52411ce2b6b105ecb51eee60f5adb2c297544cd2
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-09 18:11:49 -0300

    refactor: Update Body1 component to use HTML5 semantic tag

--- src/components/typography/body/body4.tsx
+++ src/components/typography/body/body4.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
-import { typography } from "../../../typography/typography";
+import { typography } from "../../../typography";
 
 function Body4({ children }: { children: ReactNode }) {
-  return <body style={typography.body.body4}>{children}</body>;
+  return <p style={typography.body.body4}>{children}</p>;
 }
 export default Body4;
