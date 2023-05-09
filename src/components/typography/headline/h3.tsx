c64b7ed903c3ee3584421985dc73bbdab9509e5d
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-09 16:32:08 -0300

    feat(typography): creating h3 component

---
+++ src/components/typography/headline/h3.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+import { typography } from "../../../typography/typography";
+
+function Heading3({ children }: { children: ReactNode }) {
+  return <h3 style={typography.H3}>{children}</h3>;
+}
+export default Heading3;
