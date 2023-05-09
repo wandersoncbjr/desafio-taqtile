9bab549925ab265d345b8f1c8b1e1281b7b6daa8
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-09 16:31:40 -0300

    feat(typography): creating h1 component

---
+++ src/components/typography/headline/h1.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+import { typography } from "../../../typography/typography";
+
+function Heading1({ children }: { children: ReactNode }) {
+  return <h1 style={typography.H1}>{children}</h1>;
+}
+export default Heading1;
