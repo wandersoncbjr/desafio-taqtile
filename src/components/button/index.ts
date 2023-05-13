32013c605dacba6712885ad22f7d37c2d8099bbb
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-13 15:29:15 -0300

    feat(button): add button component with dynamic styling

---
+++ src/components/button/index.ts
@@ -0,0 +1,15 @@
+export const constantsButton = {
+  primary: {
+    height: "48px",
+    width: "143px",
+    left: "20px",
+    top: "20px",
+    radius: "4px",
+    defaultCompactHeight: "40px",
+    defaultHeight: "48px",
+    size: "16px",
+    weight: 700,
+    border: "none",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
+  },
+};
