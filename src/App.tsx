0059b2533fd6415f24339c06c0d66f01bc287d59
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-25 12:40:53 -0300

    example home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import { ItemsCard } from './components/ItemsCard/ItemsCard';
+import img from './components/ItemsCard/garagem.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
+      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
+      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
+      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
+      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
+    </div>
+  );
 }
 
 export default App;
