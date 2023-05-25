7a041d1641d1d42345c1207c4fd4e96f95964ddb
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-25 13:22:58 -0300

    example home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Input } from './components/form/input';
+import img from './components/form/Vector@2x.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Input placeholder="Digite sua senhad" label="senha" icon={<img src={img} />}></Input>
+    </div>
+  );
 }
 
 export default App;
