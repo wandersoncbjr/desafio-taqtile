3df9bc9563753f53b960b572b8624757d20a3a09
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-26 23:41:44 -0300

    fix: caption

--- src/components/typography/caption/caption.tsx
+++ src/components/typography/caption/caption.tsx
@@ -2,14 +2,18 @@ import { ReactNode } from 'react';
 import { constants } from '../../../typography';
 import { colors } from '../../../typography/colors';
 
-const captionStyle = {
-  fontFamily: constants.font.family.primary,
-  fontWeight: constants.font.weight.regular,
-  fontSize: constants.font.size.small,
-  lineHeight: constants.font.lineHeight.small,
-  color: colors.NeutralXdark,
-};
+interface PropsCaption {
+  color: 'black' | 'white';
+  children: ReactNode;
+}
 
-export function Caption({ children }: { children: ReactNode }) {
+export function Caption({ children, color }: PropsCaption) {
+  const captionStyle = {
+    fontFamily: constants.font.family.primary,
+    fontWeight: constants.font.weight.regular,
+    fontSize: constants.font.size.small,
+    lineHeight: constants.font.lineHeight.small,
+    color: color === 'black' ? colors.NeutralDark : colors.Neutral,
+  };
   return <p style={captionStyle}>{children}</p>;
 }
