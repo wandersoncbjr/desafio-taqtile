895bd310191531690e7f1326853e16b19e2ef99d
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-26 23:52:51 -0300

    remove app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import './App.css';
-import { ItemsCard } from './components/ItemsCard/ItemsCard';
-import img from './components/ItemsCard/garagem.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
-      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
-      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
-      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
-      <ItemsCard title="1 vaga" icon={<img src={img} />}></ItemsCard>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
