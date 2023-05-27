554d525f3835f91f8365b0274dbc7e4a540b8347
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-26 23:57:24 -0300

    remove app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
-import { Input } from './components/form/input';
-import img from './components/form/Vector@2x.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <Input placeholder="Digite sua senhad" label="senha" icon={<img src={img} />}></Input>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
