acd5471a2e4eaaf65a3ac4a7e0b9dc6cdbffa0f4
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-27 17:22:04 -0300

    exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Card } from './components/cardCategory/card';
+import Img from './components/cardCategory/svg.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card image={<img src={Img} />} title="Com piscina" description="723,457 Imóveis"></Card>
+    </div>
+  );
 }
 
 export default App;
