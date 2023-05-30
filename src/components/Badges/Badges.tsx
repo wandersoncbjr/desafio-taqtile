701de450057ff60b16c63b253d48158f52ac76ae
wanderson santos <101597114+wandersoncbjr@users.noreply.github.com>
2023-05-30 13:48:22 -0300

    Update src/components/Badges/Badges.tsx
    Co-authored-by: Alan Raso <alanrraso@gmail.com>

--- src/components/Badges/Badges.tsx
+++ src/components/Badges/Badges.tsx
@@ -32,7 +32,7 @@ export function Badge({ title, icon, onClick, selected }: BadgeProps) {
       className="container-badges"
       onClick={handleClick}
       style={badgesStyle}
-      onMouseEnter={mouseEnter}
+      onMouseEnter={handleMouseEnter}
       onMouseLeave={mouseLeave}
     >
       <div className="icon">{icon}</div>
