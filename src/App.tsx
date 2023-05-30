55e4dccaa1cb2471519ab426eba30500db91e012
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-29 23:13:07 -0300

    exemple Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import { CardCategory } from './components/cards/cardCategory';
+import { CardProperties } from './components/cards/cardProperties';
+import Img from './components/cards/cardProperties/img.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faBed, faToilet, faRulerCombined } from '@fortawesome/free-solid-svg-icons';
+import { colors } from './typography/colors';
+
+const icons = [
+  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faBed} />, text: '3 quartos' },
+  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faToilet} />, text: '2 banheiros' },
+  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faRulerCombined} />, text: '5x7 m²' },
+];
 
 function App() {
   return (
     <div className="App">
-      <CardCategory title="Com piscina" description="723,457 Imóveis" />
+      <CardProperties
+        icon={<FontAwesomeIcon icon={faHeart} />}
+        icons={icons}
+        img={<img src={Img} />}
+        price="R$400.000"
+        title="Alphaville, SP"
+        description="Barueri, 32394, São Paulo"
+      ></CardProperties>
     </div>
   );
 }
