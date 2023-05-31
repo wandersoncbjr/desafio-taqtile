d3840f33e7c0cf6eddb416c12a615adf439540e5
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-31 20:03:52 -0300

    fix:  Card Properties

--- src/components/cards/cardProperties/index.tsx
+++ src/components/cards/cardProperties/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faToilet, faRulerCombined } from '@fortawesome/free-solid-svg-icons';
+import { ContainerCard } from '../containerCard';
+import { colors } from '../../../typography/colors';
+import { Price } from '../../typography/price/price';
 import { Body2 } from '../../typography/body/body2';
 import { Caption } from '../../typography/caption/caption';
-import { Price } from '../../typography/price/price';
-import { ContainerCard } from '../containerCard';
 import './index.css';
+
+interface CategoryProps {
+  category: string;
+  detail: string;
+}
 
 interface ContainerProps {
   img?: React.ReactNode;
@@ -11,20 +19,32 @@ interface ContainerProps {
   title: string;
   description: string;
   icon: React.ReactNode;
-  icons?: { icon: React.ReactNode; text: string }[];
+  icons?: CategoryProps[];
 }
 
 export function CardProperties({ img, price, title, description, icon, icons }: ContainerProps) {
+  const renderIcon = (category: string) => {
+    if (category === 'bathroom') {
+      return <FontAwesomeIcon color={colors.PrimaryDark} icon={faToilet} />;
+    } else if (category === 'room') {
+      return <FontAwesomeIcon color={colors.PrimaryDark} icon={faBed} />;
+    } else if (category === 'square-meter') {
+      return <FontAwesomeIcon color={colors.PrimaryDark} icon={faRulerCombined} />;
+    } else {
+      return null;
+    }
+  };
+  const formattedPrice = String(price.toFixed(2));
   return (
     <ContainerCard>
       {img}
       <div className="container-content">
         <div className="container-price-icon">
-          <Price type="medium">{`R$${price}`}</Price>
+          <Price type="medium">{`R$${formattedPrice}`}</Price>
           {icon}
         </div>
         <Body2 type="bold">{title}</Body2>
-        <Caption>{description}</Caption>
+        <Caption color="Neutral/X-dark">{description}</Caption>
       </div>
       <hr
         style={{
@@ -34,13 +54,12 @@ export function CardProperties({ img, price, title, description, icon, icons }: 
           border: 'none',
         }}
       />
-
       <div className="card-icons">
         {icons &&
           icons.map((item, index) => (
             <div className="card-icon" key={index}>
-              {item.icon}
-              <div>{item.text}</div>
+              {renderIcon(item.category)}
+              <Caption color="Neutral/X-dark">{item.detail}</Caption>
             </div>
           ))}
       </div>
--- src/components/typography/caption/caption.tsx
+++ src/components/typography/caption/caption.tsx
@@ -2,15 +2,29 @@ import { ReactNode } from 'react';
 import { constants } from '../../../typography';
 import { colors } from '../../../typography/colors';
 
+interface CaptionProps {
+  color?: 'black' | 'white' | 'Neutral/X-dark';
+  children: ReactNode;
+}
+
 const captionStyle = {
   fontFamily: constants.font.family.primary,
   fontWeight: constants.font.weight.regular,
   fontSize: constants.font.size.small,
   lineHeight: constants.font.lineHeight.small,
-  color: colors.NeutralXdark,
   margin: 0,
 };
 
-export function Caption({ children }: { children: ReactNode }) {
-  return <p style={captionStyle}>{children}</p>;
+export function Caption({ children, color }: CaptionProps) {
+  return (
+    <p
+      style={{
+        ...captionStyle,
+        color:
+          color === 'black' ? colors.NeutralDark : color === 'Neutral/X-dark' ? colors.NeutralXdark : colors.Neutral,
+      }}
+    >
+      {children}
+    </p>
+  );
 }
