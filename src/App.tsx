bfadfedf629eaf4fea5da4ba71bbeb4d9311083c
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-05-31 20:02:41 -0300

    fix: exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import './App.css';
 import { CardProperties } from './components/cards/cardProperties';
-import Img from './components/cards/cardProperties/img.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import { faBed, faToilet, faRulerCombined } from '@fortawesome/free-solid-svg-icons';
-import { colors } from './typography/colors';
+import img from './components/cards/cardProperties/Svg.svg';
 
 const icons = [
-  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faBed} />, text: '3 quartos' },
-  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faToilet} />, text: '2 banheiros' },
-  { icon: <FontAwesomeIcon color={colors.PrimaryDark} icon={faRulerCombined} />, text: '5x7 m²' },
+  { category: 'bathroom', detail: '3 quartos' },
+  { category: 'room', detail: '2 banheiros' },
+  { category: 'square-meter', detail: '5x7 m²' },
 ];
 
 function App() {
   return (
     <div className="App">
       <CardProperties
-        icon={<FontAwesomeIcon icon={faHeart} />}
+        img={<img src={img} />}
         icons={icons}
-        img={<img src={Img} />}
-        price={4000}
+        icon={<FontAwesomeIcon icon={faHeart} />}
+        price={20000.01}
         title="Alphaville, SP"
         description="Barueri, 32394, São Paulo"
-      ></CardProperties>
+      />
     </div>
   );
 }
