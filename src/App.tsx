7b97d13257ac70ac3a6bf26259b353ba3209593d
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-06-06 19:57:06 -0300

    fix: exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
 import { CardBooking } from './components/cards/card-booking';
-import img from './components/cards/card-booking/img.svg';
+import imageUser from './components/cards/card-booking/imageUser.svg';
 
 const data = {
-  priceCondominium: '1000',
-  priceIptu: '1000',
-  priceServiceFee: '1000',
-  priceGrossRent: '1000',
-  total: '2900',
+  priceCondominium: 1000,
+  priceIptu: 1000,
+  priceServiceFee: 1000,
+  priceGrossRent: 1000,
+  pricetotal: 1000,
 };
 
 function App() {
   return (
     <div className="App">
       <CardBooking
-        image={<img src={img} />}
+        imageUser={<img src={imageUser} />}
         prices={data}
         nameUser="Jonathan Marques"
         broker="Imobilar Corretora Imobiliária"
         type="rent"
+        checked={false}
       />
 
       <CardBooking
-        image={<img src={img} />}
+        checked={true}
+        imageUser={<img src={imageUser} />}
         prices={data}
         nameUser="Jonathan Marques"
         broker="Imobilar Corretora Imobiliária"
