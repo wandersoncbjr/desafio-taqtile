327259c2b58d7cc6641c4b504bcab89c11c60b54
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-06-07 19:32:30 -0300

    feat: exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import { CardCategory } from './components/cards/card-category/card-category';
+import { Carrousel } from './components/carousel-properties/carousel';
+import photoProperties from './components/carousel-properties/photo-properties.svg';
+
+const dataCarousel = [
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+  <CardCategory title="Com piscina" description="723,457 Imóveis" image={<img src={photoProperties} />} />,
+];
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{<Carrousel items={dataCarousel} />}</div>;
 }
 
 export default App;
