9ab8c1c352a43a97db13a16ca9deb23539ff3658
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-06-30 20:43:45 -0300

    fix: banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -5,6 +5,7 @@ import './banner.css';
 import banner from './banner.svg';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { colors } from '../../typography/colors';
+import { Separator } from '../separator/separatos';
 
 const handleBannerClick = () => {
   alert('Em construção');
@@ -14,16 +15,16 @@ export function Banner() {
   return (
     <div className="container-banner" onClick={handleBannerClick}>
       <img src={banner} alt="" />
-      <div style={{ margin: '16px' }}></div>
+      <Separator size="medium" />
       <div className="text-banner">
         <Display color="Neutral">Melhores imóveis</Display>
         <Body1 color="neutral">Confira opções que podem ser uma grande oportunidade!</Body1>
       </div>
-      <div style={{ margin: '16px' }}></div>
+      <Separator size="medium" />
       <div>
         <FontAwesomeIcon icon={faArrowRight} color={colors.Neutral}></FontAwesomeIcon>
       </div>
-      <div style={{ margin: '16px' }}></div>
+      <Separator size="medium" />
     </div>
   );
 }
