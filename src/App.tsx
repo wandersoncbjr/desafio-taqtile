e6e5bfe70ba9f1c10f9d78383fc78339c128cc4f
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-06-30 21:09:43 -0300

    fix: Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { CarouselSection } from './components/carousel-section/carousel-section';
+import { Hero } from './components/hero/hero';
 
 export function App() {
   return (
-    <div className="App">
+    <div>
+      <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} />
     </div>
   );
