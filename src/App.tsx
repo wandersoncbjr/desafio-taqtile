f0780257a8064ec9474be2068e8e6524ab28a35e
wanderson santos <101597114+wandersoncbjr@users.noreply.github.com>
2023-07-03 20:56:55 -0300

    Revert "Feature/banner component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { CarouselSection } from './components/carousel-section/carousel-section';
 import { Hero } from './components/hero/hero';
-import { Banner } from './components/banner/banner';
 
 export function App() {
   return (
     <div>
       <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} />
-      <Banner />
     </div>
   );
 }
--- src/components/typography/body/body-primary.tsx
+++ src/components/typography/body/body-primary.tsx
@@ -2,23 +2,16 @@ import { ReactNode } from 'react';
 import { constants } from '../../../typography';
 import { colors } from '../../../typography/colors';
 
-interface BodyProps {
-  color?: 'neutral' | 'dark';
-  children: ReactNode;
-}
-
 const bodyStyle = {
-  fontFamily: constants.font.family.primary,
-  fontWeight: constants.font.weight.regular,
-  fontSize: constants.font.size.large,
-  lineHeight: constants.font.lineHeight.small,
-  margin: 0,
+  body: {
+    fontFamily: constants.font.family.primary,
+    fontWeight: constants.font.weight.regular,
+    fontSize: constants.font.size.large,
+    lineHeight: constants.font.lineHeight.small,
+    color: colors.NeutralXdark,
+  },
 };
 
-export function Body1({ children, color }: BodyProps) {
-  const styles = {
-    ...bodyStyle,
-    color: color === 'neutral' ? colors.Neutral : colors.NeutralXdark,
-  };
-  return <p style={styles}>{children}</p>;
+export function Body1({ children }: { children: ReactNode }) {
+  return <p style={bodyStyle.body}>{children}</p>;
 }
