74647ed113bac72275f1dff1b3b6fd897edba78e
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-04 19:14:28 -0300

    fix: visual bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { Hero } from './components/hero/hero';
 import { Banner } from './components/banner/banner';
 import './App.css';
 import { Footer } from './components/footer/footer';
+import { Separator } from './components/separator/separatos';
 
 export function App() {
   return (
     <div>
       <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} />
+      <Separator size="large" />
       <Banner />
+      <Separator size="large" />
       <Footer />
     </div>
   );
