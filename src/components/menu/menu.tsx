e7fe81691220c6a252827517acbecf74bd06e5a2
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-05 22:00:16 -0300

    feat: add component menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,15 +1,34 @@
+import { Button } from '../button/button';
+import { Separator } from '../separator/separatos';
+import { Label } from '../typography/label/label';
 import { ItemMenu } from './item-menu';
 import logo from './logo.svg';
+import './menu.css';
 
 export function Menu() {
   return (
     <header>
-      <img
-        src={logo}
-        alt="logo da empresa. Imobilar
+      <div className="container-header">
+        <img
+          src={logo}
+          alt="logo da empresa. Imobilar
             Corretora Imobiliária"
-      />
-      <ItemMenu />
+        />
+        <div className="container-item-menu">
+          <ItemMenu redirect="/home" title="Home" />
+          <Separator size="medium" />
+          <ItemMenu redirect="/quem-somos" title="Quem Somos" />
+          <Separator size="medium" />
+          <ItemMenu redirect="/contato" title="Contato" />
+        </div>
+        <div className="container-button">
+          <Button variant="secondary" compact={true}>
+            <Label type="regular">Logar</Label>
+          </Button>
+          <Separator size="small" />
+          <Button compact={true}>Cadastrar</Button>
+        </div>
+      </div>
     </header>
   );
 }
