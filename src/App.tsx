42dd343529d6307d3156a30db2f5b4e22f282f99
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-10 19:23:35 -0300

    feat: add exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { CarouselSection } from './components/carousel-section/carousel-section';
-import { Hero } from './components/hero/hero';
-import { Banner } from './components/banner/banner';
-import { Menu } from './components/menu/menu';
-import { Separator } from './components/separator/separatos';
-import { Footer } from './components/footer/footer';
+import { SectionImmobile } from './components/carousel-section-immobile/carousel-section-immobile';
 
 export function App() {
   return (
     <div>
-      <Menu />
-      <Separator size="large" />
-      <Hero />
-      <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} type="category" />
-      <Separator size="large" />
-      <Banner />
-      <CarouselSection title={'Lançamentos que você pode se interessar!'} type="property" />
-      <Separator size="large" />
-      <Footer />
+      <SectionImmobile />
     </div>
   );
 }
