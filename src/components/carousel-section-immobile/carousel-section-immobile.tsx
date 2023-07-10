94cb119f690d85052d7b7095238fbd343f5aee6e
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-10 19:22:15 -0300

    feat: add component  SectionImmobile

---
+++ src/components/carousel-section-immobile/carousel-section-immobile.tsx
@@ -0,0 +1,21 @@
+import { CarouselPhotos } from '../carousel-phostos/carousel-photo';
+
+const ImmobileData = {
+  imageUrls: [
+    'https://storage.googleapis.com/instituto-taqtile/desafio/property1_1.png',
+    'https://storage.googleapis.com/instituto-taqtile/desafio/property1_2.png',
+    'https://storage.googleapis.com/instituto-taqtile/desafio/property1_3.png',
+    'https://storage.googleapis.com/instituto-taqtile/desafio/property1_3.png',
+    'https://storage.googleapis.com/instituto-taqtile/desafio/property1_3.png',
+  ],
+};
+
+export function SectionImmobile() {
+  return (
+    <CarouselPhotos>
+      {ImmobileData.imageUrls.map((img) => (
+        <img src={img}></img>
+      ))}
+    </CarouselPhotos>
+  );
+}
