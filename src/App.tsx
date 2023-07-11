514e123f1f36c12d46d850c5d8532bc04dd6101f
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-11 15:18:43 -0300

    feat: exemple home

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Hero } from './components/hero/hero';
 import { CarouselSection } from './components/carousel-section/carousel-section';
 import { Banner } from './components/banner/banner';
 import { Footer } from './components/footer/footer';
+import { SectionImmobile } from './components/description-immobile/section-immobile';
+import './App.css';
 
 export function App() {
   return (
@@ -15,11 +17,11 @@ export function App() {
       <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} type="category" />
       <Separator size="large" />
+      <Footer />
       <Banner />
       <CarouselSection title={'Lançamentos que você pode se interessar!'} type="property" />
       <Separator size="large" />
       <SectionImmobile />
-      <Footer />
     </div>
   );
 }
