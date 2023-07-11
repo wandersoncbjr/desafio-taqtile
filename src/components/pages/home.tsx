3d5cd8317deaa9f4e47ba146971cde11696b4c07
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-11 18:38:32 -0300

    fix: home

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,11 +2,14 @@ import { Banner } from '../banner/banner';
 import { CarouselSection } from '../carousel-section/carousel-section';
 import { Footer } from '../footer/footer';
 import { Hero } from '../hero/hero';
+import { Menu } from '../menu/menu';
 import { Separator } from '../separator/separatos';
 
 export function Home() {
   return (
     <>
+      <Menu />
+      <Separator size="large" />
       <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} type="category" />
       <Separator size="large" />
