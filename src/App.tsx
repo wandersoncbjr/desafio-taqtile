7b90b81f327117780fb1f2ba886283ddbbbb5ea3
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-14 20:36:59 -0300

    fix: remove App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import { SectionImmobile } from './components/carousel-section-immobile/carousel-section-immobile';
-import { Menu } from './components/menu/menu';
-import { Separator } from './components/separator/separatos';
-import { Hero } from './components/hero/hero';
-import { CarouselSection } from './components/carousel-section/carousel-section';
-import { Banner } from './components/banner/banner';
-import { Footer } from './components/footer/footer';
 
 import './App.css';
 
 export function App() {
-  return (
-    <div>
-      <Menu />
-      <Separator size="large" />
-      <Hero />
-      <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} type="category" />
-      <Separator size="large" />
-      <Footer />
-      <Banner />
-      <CarouselSection title={'Lançamentos que você pode se interessar!'} type="property" />
-      <Separator size="large" />
-      <SectionImmobile />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
