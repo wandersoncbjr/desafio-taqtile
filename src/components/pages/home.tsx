4c8e0801cf793c53c50263db1c7ffd81c1c1563f
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-14 20:47:22 -0300

    fix: home

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -3,7 +3,6 @@ import { CarouselSection } from '../carousel-section/carousel-section';
 import { Footer } from '../footer/footer';
 import { Hero } from '../hero/hero';
 import { Menu } from '../menu/menu';
-import { SectionItems } from '../section-items-immobile.tsx/section-items-immobile';
 import { Separator } from '../separator/separatos';
 
 export function Home() {
@@ -14,8 +13,6 @@ export function Home() {
       <Hero />
       <CarouselSection title={'Encontre o imóvel ideal para o seu estilo de vida!'} type="category" />
       <Separator size="large" />
-      <SectionItems title="O que esse lugar oferece" variant="Immobile" />
-      <SectionItems title="Disponível no condomínio" variant="condominium" />
       <Banner />
       <CarouselSection title={'Lançamentos que você pode se interessar!'} type="property" />
       <Separator size="large" />
