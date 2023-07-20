e4f3fef2ba4c533435aef5baa4ac512a266bffee
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-20 15:19:52 -0300

    fix: resolving conflict

--- src/components/pages/details.tsx
+++ src/components/pages/details.tsx
@@ -4,7 +4,7 @@ import { Footer } from '../footer/footer';
 import { Location } from '../location/location';
 import { Menu } from '../menu/menu';
 import { Proximities } from '../proximities/proximities';
-import { SectionItems } from '../section-items-immobile.tsx/section-items-immobile';
+import { SectionItems } from '../section-items-immobile/section-items-immobile';
 
 export function PropertyDetails() {
   return (
