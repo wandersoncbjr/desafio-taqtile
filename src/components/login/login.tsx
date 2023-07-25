060bc30f8040352ff0ab9f4c9d10b3a4347b2ee6
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-25 01:25:55 -0300

    feat: add component login

---
+++ src/components/login/login.tsx
@@ -0,0 +1,68 @@
+import { Button } from '../button/button';
+import { ContainerForm } from '../container-form/container-form';
+import { Input } from '../form/form';
+import { Separator } from '../separator/separator';
+import { TitleSubTitleForm } from '../title-subtitle-form/title-subtitle';
+import { validateEmail, validatePassword } from './validate-login';
+import React, { useState } from 'react';
+
+export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const validate = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!validateEmail.test(email)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    if (!validatePassword.test(password)) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
+  return (
+    <form onSubmit={validate}>
+      <ContainerForm>
+        <div style={{ width: '30%', zIndex: '1' }}>
+          <Separator size="large" />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <TitleSubTitleForm
+              title="Acesse sua conta"
+              subTitle="Para utilizar os serviços da nossa plataforma, você precisa entrar uma conta!"
+            />
+          </div>
+          <Separator size="medium" />
+          <Input
+            placeholder="Digite Seu Email"
+            caption="E-mail inválido."
+            error={emailError}
+            type="text"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Separator size="small" />
+          <Input
+            caption={'Senha inválida.'}
+            error={passwordError}
+            placeholder="Digite Sua Senha"
+            type="password"
+            label="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Separator size="large" />
+          <Button expanded>Entrar</Button>
+        </div>
+      </ContainerForm>
+    </form>
+  );
+}
