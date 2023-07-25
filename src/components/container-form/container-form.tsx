693833abb0dbc1cbcb0c3ade07d320d52c52cedd
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-25 01:24:06 -0300

    feat: add component ContainerForm

---
+++ src/components/container-form/container-form.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+import imgHeaderForm from './img-header.svg';
+
+interface ContainerFormProps {
+  children: ReactNode;
+}
+
+export function ContainerForm({ children }: ContainerFormProps) {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {children}
+        <div
+          style={{
+            zIndex: '0',
+            display: 'flex',
+            justifyContent: 'end',
+            width: '100%',
+            bottom: '0',
+            position: 'fixed',
+          }}
+        >
+          <img src={imgHeaderForm} />
+        </div>
+      </div>
+    </>
+  );
+}
