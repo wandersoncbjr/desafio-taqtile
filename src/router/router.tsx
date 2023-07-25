4cb6115eb3b3c9a5ef596d9d7db25645aae144b9
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-25 01:27:38 -0300

    fix: import LoginPage

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,7 +3,7 @@ import { Home } from '../components/pages/home';
 import { Contact } from '../components/pages/contact';
 import { WhoWeAre } from '../components/pages/who-we-are';
 import { PropertyDetails } from '../components/pages/details';
-import { LoginPage } from '../components/pages/login';
+import { LoginPage } from '../components/pages/login-page';
 
 const router = createBrowserRouter([
   {
