e5fb2e59ef93a25881675d580811bfb999bf2ae0
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-07-26 16:03:39 -0300

    feat: add integration in compenent SectionDescriptionImmobile

--- src/components/description-immobile/section-immobile.tsx
+++ src/components/description-immobile/section-immobile.tsx
@@ -8,39 +8,19 @@ import { ItemsCard } from '../items-card/items-card';
 import { faBath, faBed, faCar, faRulerCombined, faWaterLadder } from '@fortawesome/free-solid-svg-icons';
 import { Heading2 } from '../typography/headline/h2';
 import { Separator } from '../separator/separator';
+import { PropertyDetailsData } from '../../graphql/queries/property-details';
 
-const sectionImmbileData = {
-  data: {
-    recentProperties: [
-      {
-        bathrooms: 2,
-        bedrooms: 2,
-        area: '8x10 m²',
-        shortDescription: 'Casa à venda em Osasco, bairro Bonfim, melhor da região!',
-        description:
-          'A casa se insere em um clima de paz, de natureza. A decoração acompanha a magia desse microclima ecológico que encanta a adultos e crianças. A casa foi construída em madeira nobre. Decorada com lustres temáticos e estruturas que dão vida ao lugar. Ideal para todos que busquem descansar ou trabalhar e desejam morar na melhor região de Osasco - SP.',
-        address: {
-          street: 'Avenida das Nações Unidas',
-          streetNumber: '3239',
-          complement: 'Complemento 245B',
-          district: 'Bonfim',
-          city: 'Osasco',
-        },
-        hasPool: true,
-        hasGarage: true,
-      },
-    ],
-  },
-};
-export function SectionDescriptionImmobile() {
-  const property = sectionImmbileData.data.recentProperties[0];
+interface SectionDescriptionImmobileProps {
+  property: PropertyDetailsData;
+}
 
+export function SectionDescriptionImmobile({ property }: SectionDescriptionImmobileProps) {
   return (
     <div className="container-section-immobile">
       <Display color="NeutralXdark">{property.shortDescription}</Display>
       <div className="location">
         <img src={location} alt="Location" />
-        <BodySecondary>{`${property.address.street} ${property.address.streetNumber}, ${property.address.complement}, ${property.address.district}, ${property.address.city}`}</BodySecondary>
+        <BodySecondary>{`${property.address.street}, ${property.address.streetNumber}, ${property.address.complement}, ${property.address.district}, ${property.address.city}`}</BodySecondary>
       </div>
       <Separator size="small" />
       <ContainerCard marginAll>
